f8b82f2306b28fa1baeda077bd26a62153e6c99f
PiyushGaurav <piyushgaurav07@gmail.com>
2024-06-06 16:12:41 +0530

    added breadcrumb ui

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,18 @@
+import { RiArrowRightWideLine } from 'react-icons/ri';
+
 export default function Main() {
-	return <div>Main</div>;
+	return (
+		<>
+			<div className="info-bar">
+				<section className="breadcrumbs">
+					<a href="#" className="breadcrums__link">
+						Facebook posts
+					</a>
+					<RiArrowRightWideLine className="breadcrumbs__icon" />
+					<span className="breadcrumbs__name">Happy hour-friday</span>
+				</section>
+				<button className="button">Publish</button>
+			</div>
+		</>
+	);
 }
