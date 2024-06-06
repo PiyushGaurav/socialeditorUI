558d5b9a012531fb18b2206f11d6394ebc43ab07
PiyushGaurav <piyushgaurav07@gmail.com>
2024-06-06 15:53:58 +0530

    panel ui

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,3 +1,47 @@
+import { RiArrowLeftDoubleFill } from 'react-icons/ri';
+import { FaPlus } from 'react-icons/fa6';
+
 export default function Panel() {
-	return <div>Panel</div>;
+	return (
+		<>
+			<div className="panel__top">
+				<h1 className="first-heading">Images</h1>
+				<RiArrowLeftDoubleFill className="panel__collapse-icon" />
+			</div>
+			<div className="panel__images">
+				<img
+					className="panel__img"
+					src="https://images.unsplash.com/photo-1543096222-72de739f7917?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&fit=crop&w=280&q=100"
+					alt="Uploaded image"
+				/>
+
+				<img
+					className="panel__img"
+					src="https://images.unsplash.com/photo-1475598322381-f1b499717dda?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&fit=crop&h=280&q=100"
+					alt="Uploaded image"
+				/>
+
+				<img
+					className="panel__img"
+					src="https://images.unsplash.com/photo-1615887023516-9b6bcd559e87?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&fit=crop&w=280&q=100"
+					alt="Uploaded image"
+				/>
+
+				<img
+					className="panel__img"
+					src="https://images.unsplash.com/photo-1615749190340-34c7c3b16784?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&fit=crop&w=280&q=100"
+					alt="Uploaded image"
+				/>
+
+				<img
+					className="panel__img"
+					src="https://images.unsplash.com/photo-1615707547992-93435f1e7f13?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&fit=crop&w=280&q=100"
+					alt="Uploaded image"
+				/>
+				<button className="panel__btn">
+					<FaPlus className="panel__upload-icon" />
+				</button>
+			</div>
+		</>
+	);
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,23 +1,37 @@
+import { useState } from 'react';
 import { FaLayerGroup, FaFont, FaShapes } from 'react-icons/fa';
 import { IoImages } from 'react-icons/io5';
 
 export default function SideBar() {
+	const [selected, setSelected] = useState(2);
 	return (
 		<>
 			<section className="menu">
-				<button className="menu__button">
+				<button
+					className={`menu__button ${selected == 0 ? 'menu__button--active' : ''}`}
+					onClick={() => setSelected(0)}
+				>
 					<FaLayerGroup className="menu__icon" />
 					<span className="menu__text">Template</span>
 				</button>
-				<button className="menu__button menu__button--active">
+				<button
+					className={`menu__button ${selected == 1 ? 'menu__button--active' : ''}`}
+					onClick={() => setSelected(1)}
+				>
 					<IoImages className="menu__icon" />
 					<span className="menu__text">Images</span>
 				</button>
-				<button className="menu__button">
+				<button
+					className={`menu__button ${selected == 2 ? 'menu__button--active' : ''}`}
+					onClick={() => setSelected(2)}
+				>
 					<FaFont className="menu__icon" />
 					<span className="menu__text">Text/Fonts</span>
 				</button>
-				<button className="menu__button">
+				<button
+					className={`menu__button ${selected == 3 ? 'menu__button--active' : ''}`}
+					onClick={() => setSelected(3)}
+				>
 					<FaShapes className="menu__icon" />
 					<span className="menu__text">Shapes</span>
 				</button>
